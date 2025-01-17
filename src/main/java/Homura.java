f99c63cc7770562ab427be89ef5752935fa3eea1
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-01-17 18:11:55 +0800

    Implement logic of main()

--- src/main/java/Homura.java
+++ src/main/java/Homura.java
@@ -1,6 +1,13 @@
+import java.util.*;
+
 public class Homura {
     public final static String indent = " ".repeat(4);
     public final static String divider = "~".repeat(80) + '\n';
+
+
+    // How to read input referenced from
+    // https://github.com/Bryce-3D/My-Codeforces-Codes/blob/main/Java/0001-0100/CF_0001A.java
+    private static Scanner sc = new Scanner(System.in);
 
     public static String intro_msg() {
         String ans = indent + divider
@@ -10,6 +17,10 @@ public class Homura {
             + indent + " Bye. Hope to see you again soon!\n"
             + indent + divider;
         return ans;
+    }
+
+    public static String bye_msg() {
+        return "TODO";
     }
 
     public static String format_echo(String s) {
@@ -22,5 +33,15 @@ public class Homura {
 
     public static void main(String[] args) {
         System.out.println(intro_msg());
+        while (true) {
+            // How to read input referenced from
+            // https://github.com/Bryce-3D/My-Codeforces-Codes/blob/main/Java/0001-0100/CF_0001A.java
+            String inp = sc.nextLine();
+            if (inp.equals("bye")) {
+                System.out.println(bye_msg());
+                break;
+            }
+            System.out.println(format_echo(inp));
+        }
     }
 }
