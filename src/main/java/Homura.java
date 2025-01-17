045ce3a8846de3304d02ac21a6db17537db71dc6
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-01-17 16:36:00 +0800

    Create Homura.java based on Duke.java

---
+++ src/main/java/Homura.java
@@ -0,0 +1,13 @@
+public class Homura {
+    public final static String divider = "~".repeat(80) + '\n';
+
+    public static void main(String[] args) {
+        String msg = divider
+                + " Hello! I'm 暁美ほむら\n"
+                + " What can I do for you?\n"
+                + divider
+                + " Bye. Hope to see you again soon!\n"
+                + divider;
+        System.out.println(msg);
+    }
+}
