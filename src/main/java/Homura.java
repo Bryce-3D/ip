08b90e963c8c32635ff21cf8af086a1d5713534a
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-01-17 16:51:04 +0800

    Create function to format echo string

--- src/main/java/Homura.java
+++ src/main/java/Homura.java
@@ -1,5 +1,14 @@
 public class Homura {
+    public final static String indent = " ".repeat(4);
     public final static String divider = "~".repeat(80) + '\n';
+    
+    public static String format_echo(String s) {
+        String ans = indent + divider
+            + indent + s + '\n'
+            + indent + divider
+            + '\n';
+        return ans;
+    }
 
     public static void main(String[] args) {
         String msg = divider
