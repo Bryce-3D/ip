37adfc717414a822fa8fc5e2b8d5a244dbfd972b
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-01-21 21:29:31 +0800

    Create HomuraRuntimeException and InvalidInputException

---
+++ src/main/java/HomuraRuntimeException.java
@@ -0,0 +1,9 @@
+public class HomuraRuntimeException extends RuntimeException {
+    public HomuraRuntimeException() {
+        super();
+    }
+
+    public HomuraRuntimeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/InvalidInputException.java
@@ -0,0 +1,18 @@
+public class InvalidInputException extends HomuraRuntimeException {
+    public static final String indent = "    ";
+
+    public String cmd;
+    public String invalid_input;
+
+    public InvalidInputException(String cmd, String invalid_input) {
+        super();
+        this.cmd = cmd;
+        this.invalid_input = invalid_input;
+    }
+
+    @Override
+    public String toString() {
+        return "java.lang.InvalidInputException: Invalid input passed to cmd " + cmd + ":" + '\n'
+            + indent + indent + invalid_input;
+    }
+}
