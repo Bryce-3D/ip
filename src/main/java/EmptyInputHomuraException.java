b201bd61e5d628bcc2c28e0f892e4cbb2232e6fa
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-01-21 22:11:15 +0800

    Implement EmptyInputHomuraException

---
+++ src/main/java/EmptyInputHomuraException.java
@@ -0,0 +1,22 @@
+public class EmptyInputHomuraException extends RuntimeException {
+    public static final String indent = "    ";
+
+    public String cmd;
+    public String inp;
+
+    public EmptyInputHomuraException() {
+        super();
+    }
+    public EmptyInputHomuraException(String cmd, String inp) {
+        super();
+        this.cmd = cmd;
+        this.inp = inp;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()
+            + " - Empty input passed to cmd " + cmd + ":" + '\n'
+            + indent + indent + inp;
+    }
+}
