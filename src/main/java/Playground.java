ede264313f40fd7bfcfb5f03f1da408b1acd0890
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-01-25 20:35:21 +0800

    Implement split() in Storage.java

--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -1,15 +1,49 @@
 // For testing code without using I Run Code Discord Bot
 // esp cause I can't fit all my classes in 2k chars
 
+import java.util.*;
+
 public class Playground {
     public static void main(String[] args) {
         Todo t = new Todo("Waow");
         Deadline d = new Deadline("stuff","now");
         Event e = new Event("You", "now", "later");
 
-        System.out.println(Storage.to_str(t));
-        System.out.println(Storage.to_str(d));
-        System.out.println(Storage.to_str(e));
+//        String ts = Storage.to_str(t);
+//        String ds = Storage.to_str(d);
+//        String es = Storage.to_str(e);
+//
+//        Todo t0 = Storage.from_str(ts);
+////        Deadline d0 = (Deadline) Storage.from_str(ds);
+////        Event e0 = (Event) Storage.from_str(es);
+//
+//        System.out.println(ts);
+//        System.out.println(ds);
+//        System.out.println(es);
+//
+//        System.out.println(t0);
+////        System.out.println(d0);
+////        System.out.println(e0);
+
+
+        String s0 = "Wait | what | ???";
+        ArrayList<String> ss = Storage.split(s0," | ");
+        for (String s : ss) {
+            System.out.println(s);
+        }
+
+
+
+
+
+//        String s0 = "Wait | what | ???";
+//        String[] ss = s0.split(" | ");
+//        for (String s : ss) {
+//            System.out.println(s);
+//        }
+//        System.out.println(ss.length);   // 5?
+//        // https://stackoverflow.com/questions/4731055/whitespace-matching-regex-java
+
 
 //        System.out.println("Test");
     }
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Storage {
-    public static final String DIVIDER = " | ";
+    public static final String DIVIDER = "|||";
 
+    // Helper Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     /**
      * Returns the contents of a file as a String.
      * Returns null if the file does not exist.
@@ -26,7 +27,6 @@ public class Storage {
             return null;
         }
     }
-
     /**
      * Writes the String to a file.
      * Creates a file if it does not exist and overwrites an
@@ -49,6 +49,45 @@ public class Storage {
         }
     }
 
+    /**
+     * Splits a string along a given divider.
+     *
+     * @param s The string being split.
+     * @param div The divider.
+     * @return The parts of `s` after being split along `div`.
+     */
+    public static ArrayList<String> split(String s, String div) {
+        // Indices where `div` appears in `s`
+        ArrayList<Integer> inds = new ArrayList<Integer>();
+        int l_s = s.length();
+        int l_d = div.length();
+
+        // Linearly scan `s` to find instances of `div`
+        int ind = 0;
+        while (ind < l_s-l_d) {
+            if (s.substring(ind,ind+l_d).equals(div)) {
+                inds.add(ind);
+                ind += l_d;
+            } else{
+                ind += 1;
+            }
+        }
+
+        // Extract the relevant substrings from `s`
+        ArrayList<String> ans = new ArrayList<String>();
+        ind = 0;
+        for (int i = 0; i < inds.size(); i++) {
+            ans.add(s.substring(ind,inds.get(i)));
+            ind = inds.get(i) + l_d;
+        }
+        // Don't forget the part after the last `div`, if any
+        if (ind < l_s) {
+            ans.add(s.substring(ind));
+        }
+        return ans;
+    }
+
+    // Todo <-> Storage String conversions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     /**
      * Convert a Todo to a String representation for storage.
      * The current format is `t | 0 or 1 | descr`
@@ -102,6 +141,34 @@ public class Storage {
         ans += DIVIDER + e.getDescription()
                 + DIVIDER + e.getSta()
                 + DIVIDER + e.getEnd();
+        return ans;
+    }
+
+    public static Todo from_str(String s) {
+        // TODO !!!!! CuRRENTLY BUGGY !!!!! TODO
+        String[] ss = s.split(DIVIDER);
+
+        for (String s0 : ss) {
+            System.out.println(s0);
+        }
+
+        Todo ans;
+        switch (ss[0]) {
+        case "t":
+            ans = new Todo(ss[3]);
+            break;
+        case "d":
+            ans = new Deadline(ss[2],ss[3]);
+            break;
+        case "e":
+            ans = new Event(ss[2],ss[3],ss[4]);
+            break;
+        default:
+            return null;
+        }
+        if (ss[1].equals("1")) {
+            ans.setIsDone(true);
+        }
         return ans;
     }
 
