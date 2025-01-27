b189d1ac7502189d3bdfc732b361647e49c895a7
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-01-27 13:06:24 +0800

    Implement Parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,44 @@
+public class Parser {
+    public static Todo parseTodoInp(String inp) {
+        inp = inp.strip();
+        if (inp.length() <= 5) {
+            throw new EmptyInputHomuraException("Todo", inp);
+        }
+        inp = inp.substring(5);   // Remove the "todo " in front
+        return new Todo(inp);
+    }
+
+    public static Deadline parseDeadlineInp(String inp) {
+        inp = inp.strip();
+        if (inp.length() <= 9) {
+            throw new EmptyInputHomuraException("Todo", inp);
+        }
+        try {
+            inp = inp.substring(9);   // Remove the "deadline " in front
+            String[] splitInps = inp.split(" /by ");
+            String descr = splitInps[0];
+            String byStr = splitInps[1];
+            return new Deadline(descr,byStr);
+        } catch (Exception e) {
+            throw new InvalidInputHomuraException("deadline", inp);
+        }
+    }
+
+    public static Event parseEventInp(String inp) {
+        inp = inp.strip();
+        if (inp.length() <= 6) {
+            throw new EmptyInputHomuraException("Todo", inp);
+        }
+        try {
+            inp = inp.substring(6);   // Remove the "event " in front
+            String[] splitInps = inp.split(" /from ");
+            String descr = splitInps[0];
+            splitInps = splitInps[1].split(" /to ");
+            String sta = splitInps[0];
+            String end = splitInps[1];
+            return new Event(descr, sta, end);
+        } catch (Exception e) {
+            throw new InvalidInputHomuraException("event", inp);
+        }
+    }
+}
