e05795e15f6d8d3420f1f9ec9c97e82b8ced243f
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-01-28 23:09:50 +0800

    Initialise a test class for Todo

---
+++ src/test/java/Homura/TodoTest.java
@@ -0,0 +1,13 @@
+package Homura;
+
+// Copied from and inspired by
+// https://se-education.org/guides/tutorials/junit.html#adding-junit-support-to-your-project
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void what() {
+        assertEquals(1,2);
+    }
+}
